d922e84c19a7f9b95eddcedaf875853ee95b9bf9
hulya.garibli <qeriblih@gmail.com>
2023-10-04 00:30:34 +0400

    commit

--- src/main/java/kodlamaio/northwind/core/dataAccess/UserDao.java
+++ src/main/java/kodlamaio/northwind/core/dataAccess/UserDao.java
@@ -4,5 +4,5 @@ import kodlamaio.northwind.core.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDao extends JpaRepository<User, Integer> {
-    //User findByEmail(String )
+    User findByEmail(String email);
 }
